646e83f80386061370fb16036d59e243d2335b93
ermiyasayeledeveloper <tiobura772@gmail.com>
2025-09-19 00:49:09 -0700

    first commit for amazon deployment

---
+++ index.js
@@ -0,0 +1,52 @@
+
+ 
+
+// const {setGlobalOptions} = require("firebase-functions");
+// const {onRequest} = require("firebase-functions/https");
+// const logger = require("firebase-functions/logger");
+
+// setGlobalOptions({ maxInstances: 10 });
+
+
+
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const stripe = require("stripe")(process.env.STRIP_KEY)
+const app = express()
+app.use(cors({origin: true}));
+
+app.use(express.json())
+
+app.get("/", (req,res) => {
+    res.status(200).json({
+        message:"success !",
+    })
+})
+
+app.get("/payment/create", async(req, res)=>{
+    const total = req.query.total;
+    if(total > 0){
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total,
+            currency: "usd",
+        });
+
+
+    res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+    });
+
+    }else{
+        res.status(403).json({
+            message:"total must be greater than 0",
+        });
+    }
+});
+
+
+app.listen(5000, (err)=>{
+    if(err) throw err
+    console.log("Amazone server running on port: 5000, http://localhost:5000")
+})
